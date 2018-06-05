1aa9d7c43c793700669df5f7880eb4b8845543a1
Guy Powell <guy@lts.ai>
2018-06-05 08:24:01 +0000

    fix typo

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -247,7 +247,16 @@ Value getblock(const Array& params, bool fHelp)
     CBlockIndex* pblockindex = mapBlockIndex[hash];
     block.ReadFromDisk(pblockindex, true);
 
-    return blockToJSON(block, pblockindex, params.size() > 1 ? params[1].get_bool() : false);
+
+    if ( params.size() > 1 ? !params[1].get_bool() : false)
+    {
+        CDataStream ssBlock(SER_NETWORK, PROTOCOL_VERSION);
+        ssBlock << block;
+        std::string strHex = HexStr(ssBlock.begin(), ssBlock.end());
+        return strHex;
+    }
+
+    return blockToJSON(block, pblockindex, true);
 }
 
 Value getblockbynumber(const Array& params, bool fHelp)
